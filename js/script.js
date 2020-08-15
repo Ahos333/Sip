fc6c412a05c717e17caf661530faaab4a6074be1
Angelica Hoskins <anghos080@gmail.com>
2020-08-15 21:50:42 +0000

    Added css, img, and js folder with respective files.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto', 
+		autoplay: {
+        delay: 2300,
+        disableOnInteraction: false,
+		  loop: true,
+      },
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
